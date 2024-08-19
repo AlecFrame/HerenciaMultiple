85f5a8c8c8422069859ecdd3c5db26343a469b68
JuanCZ55 <juancruzzegarra@gmail.com>
2024-08-19 15:33:31 -0300

    Clase Tren
    Se agrego una nueva clase llamada tren

---
+++ src/herenciamultiple/Tren.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package herenciamultiple;
+
+/**
+ *
+ * @author juanz
+ */
+public class Tren {
+    private int edad;
+
+    public Tren(int edad) {
+        this.edad = edad;
+        System.out.println("Bienvenido al Tren");
+    }
+
+}
